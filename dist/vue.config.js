e2aac664c917a83421e417687b9e3bdd57603b98
kurokaro <linfanhsin@gmail.com>
2019-12-26 09:42:30 +0800

    fix config

--- dist/vue.config.js
+++ dist/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/Vue-TodoList/'
-      : '/'
-  }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Vue-TodoList/'
+    : '/'
+}
