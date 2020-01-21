14ec54e16ae43499ddf5c303d0aed9c469c6eb3a
kurokaro <linfanhsin@gmail.com>
2020-01-21 18:40:19 +0800

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -39,7 +39,7 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
-git push -f https://github.com/Kuaruou/Vue-TodoList.git master:gh-pages
+git push -f https://github.com/Kuaruou/Vuex-TodoList.git master:gh-pages
 #//將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 
 cd -
