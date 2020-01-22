e5fe31090422b04131f6ccfbc04f74d404838d46
kurokaro <linfanhsin@gmail.com>
2020-01-22 16:00:54 +0800

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,33 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
 import Home from '../views/Home.vue'
 
-Vue.use(VueRouter)
+Vue.use(Router);
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  }
-]
+export default new Router({
+  routes :[
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+  ]
+});
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+// Vue.use(VueRouter)
 
-export default router
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'home',
+//     component: Home
+//   },
+// ]
+
+// const router = new VueRouter({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   routes
+// })
+
+// export default router
